47083837ddb7a1869ce88937c5333e3c294b8836
ne1ro <jack.20033505@gmail.com>
2024-10-30 14:00:37 +0300

    update email

--- Mates/api/urls.py
+++ Mates/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterAPIView, PasswordResetRequestView, PasswordResetConfirmView
+from .views import RegisterAPIView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
