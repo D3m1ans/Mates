eea32282f505282173f327472cedfb6d8f730935
ne1ro <jack.20033505@gmail.com>
2024-10-31 22:54:45 +0300

    Password-rest

--- Mates/Mates/settings.py
+++ Mates/Mates/settings.py
@@ -146,3 +146,32 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tmates.help@gmail.com'
 EMAIL_HOST_PASSWORD = 'wwvo anru qwjh ilze'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+API_BASE_URL = 'http://127.0.0.1:8000'
+
+
+# settings.py
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],  # Запись в консоль и в файл
+            'level': 'DEBUG',  # Устанавливаем уровень логирования на DEBUG
+        },
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+    },
+}
--- Mates/api/urls.py
+++ Mates/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterAPIView
+from .views import RegisterAPIView, PasswordResetRequestView, PasswordResetConfirmView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('register/', RegisterAPIView.as_view(), name='register_api'),
+
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset-confirm/<uid64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
